3f6745f7d45dd8d8b8718595ef88ee1b6e217bcc
Marius Meißner <info@marius-meissner.net>
2017-01-12 23:31:35 +0100

    Renamed OperatorServer + RelayService

--- run.php
+++ run.php
@@ -2,9 +2,9 @@
 use Ratchet\Http\HttpServer;
 use Ratchet\Server\IoServer;
 use Ratchet\WebSocket\WsServer;
-use Volante\SkyBukkit\RelayServer\Src\OperatorServer;
+use Volante\SkyBukkit\RelayServer\Src\Controller;
 
 require __DIR__.'/vendor/autoload.php';
 
-$server = IoServer::factory(new HttpServer(new WsServer(new OperatorServer())), 8080);
+$server = IoServer::factory(new HttpServer(new WsServer(new Controller())), 8080);
 $server->run();
