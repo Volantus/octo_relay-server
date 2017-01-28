6e6bca159aa1dd02f6f8f4716adc847b1eb5722d
Marius Meißner <info@marius-meissner.net>
2017-01-29 00:57:51 +0100

    Fixed failing tests caused by new common version

--- src/Messaging/IncomingMessageCreationService.php
+++ src/Messaging/IncomingMessageCreationService.php
@@ -2,6 +2,7 @@
 namespace Volante\SkyBukkit\RelayServer\Src\Messaging;
 
 use Volante\SkyBukkit\Common\Src\General\GeoPosition\GeoPositionMessageFactory;
+use Volante\SkyBukkit\Common\Src\General\GyroStatus\GyroStatusMessageFactory;
 use Volante\SkyBukkit\Common\Src\Server\Authentication\AuthenticationMessageFactory;
 use Volante\SkyBukkit\Common\Src\Server\Messaging\MessageService;
 use Volante\SkyBukkit\Common\Src\Server\Network\RawMessageFactory;
@@ -19,21 +20,23 @@ class IncomingMessageCreationService extends MessageService
     /**
      * IncomingMessageCreationService constructor.
      *
-     * @param RawMessageFactory|null            $rawMessageFactory
-     * @param IntroductionMessageFactory|null   $introductionMessageFactory
+     * @param RawMessageFactory|null $rawMessageFactory
+     * @param IntroductionMessageFactory|null $introductionMessageFactory
      * @param AuthenticationMessageFactory|null $authenticationMessageFactory
-     * @param GeoPositionMessageFactory|null    $geoPositionMessageFactory
-     * @param RequestTopicStatusMessageFactory  $requestTopicStatusMessageFactory
-     * @param SubscriptionStatusMessageFactory  $subscriptionStatusMessageFactory
+     * @param GeoPositionMessageFactory|null $geoPositionMessageFactory
+     * @param GyroStatusMessageFactory $gyroStatusMessageFactory
+     * @param RequestTopicStatusMessageFactory $requestTopicStatusMessageFactory
+     * @param SubscriptionStatusMessageFactory $subscriptionStatusMessageFactory
      */
     public function __construct(RawMessageFactory $rawMessageFactory = null,
         IntroductionMessageFactory $introductionMessageFactory = null,
         AuthenticationMessageFactory $authenticationMessageFactory = null,
         GeoPositionMessageFactory $geoPositionMessageFactory = null,
+        GyroStatusMessageFactory $gyroStatusMessageFactory = null,
         RequestTopicStatusMessageFactory $requestTopicStatusMessageFactory = null,
         SubscriptionStatusMessageFactory $subscriptionStatusMessageFactory = null
     ) {
-        parent::__construct($rawMessageFactory, $introductionMessageFactory, $authenticationMessageFactory, $geoPositionMessageFactory);
+        parent::__construct($rawMessageFactory, $introductionMessageFactory, $authenticationMessageFactory, $geoPositionMessageFactory, $gyroStatusMessageFactory);
 
         $this->registerFactory($requestTopicStatusMessageFactory ?: new RequestTopicStatusMessageFactory());
         $this->registerFactory($subscriptionStatusMessageFactory ?: new SubscriptionStatusMessageFactory());
