a92ea56e9bb1d5f123facf7e0da342b995496fb4
Marius Meißner <info@marius-meissner.net>
2017-02-03 02:15:51 +0100

    Removed debug info

--- src/Messaging/MessageRelayService.php
+++ src/Messaging/MessageRelayService.php
@@ -124,7 +124,6 @@ class MessageRelayService extends MessageServerService
     {
         $revision = $subscription->getRevision() + 1;
         foreach ($this->repositories[$subscription->getName()]->get($revision) as $message) {
-            $this->writeInfoLine('Debug', $message->getTopic()->getRevision());
             $client->send(json_encode($message->toRawMessage()));
             $subscription->incrementRevision();
         }
