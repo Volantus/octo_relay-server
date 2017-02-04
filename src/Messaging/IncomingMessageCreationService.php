45f15107170f603885a1c9bfb25b5392751a8389
Marius Meißner <info@marius-meissner.net>
2017-02-04 15:23:31 +0100

    Made relay-server compatible with common v1.6.0

--- src/Messaging/IncomingMessageCreationService.php
+++ src/Messaging/IncomingMessageCreationService.php
@@ -3,6 +3,7 @@ namespace Volante\SkyBukkit\RelayServer\Src\Messaging;
 
 use Volante\SkyBukkit\Common\Src\General\GeoPosition\GeoPositionMessageFactory;
 use Volante\SkyBukkit\Common\Src\General\GyroStatus\GyroStatusMessageFactory;
+use Volante\SkyBukkit\Common\Src\General\Motor\MotorStatusMessageFactory;
 use Volante\SkyBukkit\Common\Src\Server\Authentication\AuthenticationMessageFactory;
 use Volante\SkyBukkit\Common\Src\Server\Messaging\MessageService;
 use Volante\SkyBukkit\Common\Src\Server\Network\RawMessageFactory;
@@ -20,23 +21,25 @@ class IncomingMessageCreationService extends MessageService
     /**
      * IncomingMessageCreationService constructor.
      *
-     * @param RawMessageFactory|null $rawMessageFactory
-     * @param IntroductionMessageFactory|null $introductionMessageFactory
+     * @param RawMessageFactory|null            $rawMessageFactory
+     * @param IntroductionMessageFactory|null   $introductionMessageFactory
      * @param AuthenticationMessageFactory|null $authenticationMessageFactory
-     * @param GeoPositionMessageFactory|null $geoPositionMessageFactory
-     * @param GyroStatusMessageFactory $gyroStatusMessageFactory
-     * @param RequestTopicStatusMessageFactory $requestTopicStatusMessageFactory
-     * @param SubscriptionStatusMessageFactory $subscriptionStatusMessageFactory
+     * @param GeoPositionMessageFactory|null    $geoPositionMessageFactory
+     * @param GyroStatusMessageFactory          $gyroStatusMessageFactory
+     * @param RequestTopicStatusMessageFactory  $requestTopicStatusMessageFactory
+     * @param SubscriptionStatusMessageFactory  $subscriptionStatusMessageFactory
+     * @param MotorStatusMessageFactory         $motorStatusMessageFactory
      */
     public function __construct(RawMessageFactory $rawMessageFactory = null,
         IntroductionMessageFactory $introductionMessageFactory = null,
         AuthenticationMessageFactory $authenticationMessageFactory = null,
         GeoPositionMessageFactory $geoPositionMessageFactory = null,
         GyroStatusMessageFactory $gyroStatusMessageFactory = null,
+        MotorStatusMessageFactory $motorStatusMessageFactory = null,
         RequestTopicStatusMessageFactory $requestTopicStatusMessageFactory = null,
         SubscriptionStatusMessageFactory $subscriptionStatusMessageFactory = null
     ) {
-        parent::__construct($rawMessageFactory, $introductionMessageFactory, $authenticationMessageFactory, $geoPositionMessageFactory, $gyroStatusMessageFactory);
+        parent::__construct($rawMessageFactory, $introductionMessageFactory, $authenticationMessageFactory, $geoPositionMessageFactory, $gyroStatusMessageFactory, $motorStatusMessageFactory);
 
         $this->registerFactory($requestTopicStatusMessageFactory ?: new RequestTopicStatusMessageFactory());
         $this->registerFactory($subscriptionStatusMessageFactory ?: new SubscriptionStatusMessageFactory());
